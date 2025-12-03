ef89c1c89a73e43c6b182cc90823d8d725ace2b1
N3rdL0rd <168280212+N3rdL0rd@users.noreply.github.com>
2025-12-03 13:43:41 -0600

    feat(dcmod): Tweaks and QoL mods

--- mods/dcmod/globals.py
+++ mods/dcmod/globals.py
@@ -4,10 +4,14 @@ from stubs import ui
 from typing import Optional
 
 CONSOLE: Optional[ui.Console] = None
-PREDICTABLE_STAMP: bool = True # return a predictable stamp value from $PakUtils.getPakStampHash so changes to version info in the game don't invalidate your paks
-INGAME_LOGS: bool = True
-LOG_COLOR: int = 0xffffff
-CUSTOM_BUILD_TEXT: bool = True
+
+PREDICTABLE_STAMP        :bool = True      # whether or not to return a predictable stamp value from tools.PakUtils.getPakStampHash so changes to version info in the game don't invalidate your paks
+INGAME_LOGS              :bool = True      # logs dcmod and other mods' messages to the ingame debug console, and re-enables the debug logging built into the game
+LOG_COLOR                :int  = 0xffffff  # the color to use for mods' debug messages by default
+CUSTOM_BUILD_TEXT        :bool = True      # use the value in BUILD_TEXT with the build text on the main menu
+REMOVE_NEWS              :bool = True      # remove the update logo and the advertisement in the top right corner of the main menu
+SKIP_SPLASH              :bool = True      # shorten the opening splash screen (the one that displays the EE and MT logos before starting)
+HIDE_CONTROLLER_WARNING  :bool = True      # hides the "we recommend playing with a controller" warning on the main menu
 # TODO: config system
 
-BUILD_TEXT = f"dcmod {MOD_INFO["version"]}, powered by hlmod {hlmod.version}"
+BUILD_TEXT = f"dcmod {MOD_INFO["version"]} (hlmod {hlmod.version})."
--- mods/dcmod/hooks.py
+++ mods/dcmod/hooks.py
@@ -1,10 +1,13 @@
 
-from typing import Any, Optional
+from typing import Any, Callable, Optional
 from hlmod import Hook
 from modcore import hook
 from stubs import ui
 from stubs import h2d
-from stubs.pr import TitleScreen
+from stubs.pr import TitleScreen, LogoSplashscreen
+from stubs.ui import NewsPanel, Text
+from stubs import Assets
+from stubs.h2d import Object as H2DObject
 from . import globals
 from .util import log
 
@@ -24,7 +27,7 @@ def hook_console_log(self: Hook, this: ui.Console, logText: str, color: Optional
 def hook_titlescreen_setMiscTexts(self: Hook, this: TitleScreen):
     self.call_original(this)
     if globals.CUSTOM_BUILD_TEXT:
-        this.build.set_text(globals.BUILD_TEXT)
+        this.build.set_text(this.build.text + " " + globals.BUILD_TEXT)
 
 @hook("tools.pak.$PakUtils.getPakStampHash")
 def hook_pakutils_getPakStampHash(self: Hook) -> str:
@@ -38,3 +41,35 @@ def hook_logutils_log(self: Hook, text: str, severity: Any, pos: Any):
     if globals.CONSOLE:
         globals.CONSOLE.log(text, 0xb8fcf7)
     self.call_original(text, severity, pos)
+
+@hook("ui.NewsPanel.updateVisible")
+def hook_ui_NewsPanel_updateVisible(self: Hook, this: NewsPanel):
+    if not globals.REMOVE_NEWS:
+        return self.call_original(this)
+
+@hook("ui.NewsPanel.focusIn")
+def hook_ui_NewsPanel_focusIn(self: Hook, this: NewsPanel):
+    if not globals.REMOVE_NEWS:
+        return self.call_original(this)
+    
+padWarning: Optional[Text] = None
+    
+@hook("pr.TitleScreen.update")
+def hook_pr_TitleScreen_update(self: Hook, this: TitleScreen):
+    global padWarning
+    if globals.HIDE_CONTROLLER_WARNING:
+        padWarning = this.padWarning
+        padWarning.set_visible(False)
+    if globals.REMOVE_NEWS:
+        if this.isMainMenu:
+            this.updateBtn.set_visible(False)
+    self.call_original(this)
+    
+@hook("h2d.Object.set_visible")
+def hook_h2d_Object_set_visible(self: Hook, this: H2DObject, val: bool):
+    if globals.HIDE_CONTROLLER_WARNING:
+        if padWarning is None:
+            return self.call_original(this, val)
+        if this._hlmod_ptr.ptr == padWarning._hlmod_ptr.ptr: # HACK: lazy but it works
+            val = False
+    return self.call_original(this, val)
